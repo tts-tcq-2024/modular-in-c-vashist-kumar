2d366311948eca50240f80815f7a59ebc7bd4d4c
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 10:58:58 +0530

    Update colorPairDataStructures.h

--- colorPairDataStructures.h
+++ colorPairDataStructures.h
@@ -1,6 +1,8 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
@@ -8,6 +10,13 @@ const char* MajorColorNames[] = {
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct {
     enum MajorColor majorColor;
