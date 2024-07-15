2e679801657ae91a4ed7f7bcb650ae5845047196
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 11:07:21 +0530

    Update testFunctions.c

--- testFunctions.c
+++ testFunctions.c
@@ -2,23 +2,7 @@
 #include <assert.h> 
 #include <colorPairDataStructures.h>
 #include <colorPairToNumberFunctions.h>
-
-void PrintColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
-}
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
+#include <numberToColorPair.h>
 
 
 void testNumberToPair(int pairNumber,
