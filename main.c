51670107773e4ec79625a10b48c8fea3a0b63a68
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 10:49:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <assert.h> 
+#include <colorPairDataStructures.h>
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
+
 
 void PrintColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
