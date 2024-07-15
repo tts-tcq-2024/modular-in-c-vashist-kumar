7273c18a855501be602cde80734387615295fd41
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 10:59:38 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,6 @@
 #include <assert.h> 
 #include <colorPairDataStructures.h>
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-
 
 void PrintColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
