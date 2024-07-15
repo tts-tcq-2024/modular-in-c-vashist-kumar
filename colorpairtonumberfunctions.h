3053e2b13cc01dc9792ba8e2b73ba11801e13a85
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 11:00:18 +0530

    Update colorpairtonumberfunctions.h

--- colorpairtonumberfunctions.h
+++ colorpairtonumberfunctions.h
@@ -1,3 +1,5 @@
+#include <colorPairDataStructures.h>
+
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
