0a7a0e67b3366743862e6e3ef54f6e88239c4889
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 11:06:21 +0530

    Update numberToColorPair.h

--- numberToColorPair.h
+++ numberToColorPair.h
@@ -1,1 +1,10 @@
-
+#include <colorPairDataStructures.h>
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
