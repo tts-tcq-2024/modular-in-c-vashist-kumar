9533896ca8a99dac665a08aeb9077f647a3d4bc6
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-15 10:48:27 +0530

    Update colorPairDataStructures.h

--- colorPairDataStructures.h
+++ colorPairDataStructures.h
@@ -4,3 +4,7 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
