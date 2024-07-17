940267ecc5957bb9abb5b577b08d6819da4a5355
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-17 16:51:36 +0530

    Update convertColorPairToNumber.h

--- convertColorPairToNumber.h
+++ convertColorPairToNumber.h
@@ -1,6 +1,6 @@
 
 
-int GetPairNumberFromColor(const ColorPair* colorPair) {
+int GetNumberFromGivenColorPair(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
